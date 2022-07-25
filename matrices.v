d153d3f25f9ec813da63ecccbbf27ac09a1fe6cd
Alex Loiko <alexandreloiko@gmail.com>
2022-07-25 10:40:17 +0200

    Done with diagonalization of adj2 and char_poly of adj2

--- matrices.v
+++ matrices.v
@@ -23,23 +23,23 @@ Section sim_char_poly.
     unfold char_poly, char_poly_mx; rewrite conjumx //  !map_mxM.
 
     set lift := map_mx (polyC_rmorphism F).
-    have lift_invmx : lift (invmx V) = (invmx (lift V))
+    have -> : lift (invmx V) = (invmx (lift V))
       by subst lift; rewrite map_invmx.
 
     set theX := (@scalar_mx _ (S n) (polyX _)).
-    have: theX = (lift V) *m theX *m (lift (invmx V)).
+    have x_eq: theX = (lift V) *m theX *m (invmx (lift V)).
     {
       subst lift;
         (* TODO: kolla upp hur man gör när en rewrite-regel
      har premisser. Här har 'map_invmx' premisser som
 fixas ganska enkelt.
          *)
-        rewrite lift_invmx scalar_mxC  -mulmxA  mulmxV ?mulmx1 //.
+        rewrite  scalar_mxC  -mulmxA mulmxV ?mulmx1 //.
       by rewrite map_unitmx; exact Vu.
     }
-    move=> x_eq; rewrite {1}x_eq.
+    rewrite {1}x_eq.
     rewrite -mulmxBl -mulmxBr  !det_mulmx  GRing.mulrC GRing.mulrA
-                        lift_invmx det_inv mulVr ?mul1r.
+                         det_inv mulVr ?mul1r.
     by []. by rewrite -unitmxE map_unitmx ?Vu. 
   Qed.
   
@@ -131,21 +131,24 @@ Lemma det_block [n1 n2: nat] (A: 'M[R]_n1) (B: 'M_(n1, n2))
 Proof.
   clear n k nge1 kge1 field_has_char_zero.
   set block2 := (block_mx 1%:M 0 (-(pinvmx D)*m C) 1%:M).
+    
+  have block_2_det1 : (\det block2 = 1) by rewrite det_lblock !det1 mul1r.
+  have block_2_inv : block2 \in unitmx by rewrite unitmxE block_2_det1 unitr1.
+
+  rewrite -(mulmxK block_2_inv (block_mx A B C D)).
   
-  have block_eq:
+  have ->:
     (block_mx A B C D) *m block2
     = block_mx (A - B *m (pinvmx D) *m C) B 0 D
     by rewrite mulmx_block !mulmx1 !mulmx0 !add0r [B *m (_ *m _)]mulmxA
     !mulNmx !mulmxN  [D *m _]mulmxA  (pinvmxE Dinv)
-          -[D *m invmx D *m C]mulmxA (mulKVmx Dinv _) addrN mulNmx.
-  have block_2_det1 : (\det block2 = 1) by rewrite det_lblock !det1 mul1r.
-  have block_2_inv : block2 \in unitmx by rewrite unitmxE block_2_det1 unitr1.
-  by rewrite -(mulmxK block_2_inv (block_mx A B C D)) block_eq
-                det_mulmx  det_inv block_2_det1 invr1 mulr1 det_ublock mulrC.
+       -[D *m invmx D *m C]mulmxA (mulKVmx Dinv _) addrN mulNmx.
+  by rewrite det_mulmx  det_inv block_2_det1 invr1 mulr1 det_ublock mulrC.
 Qed.
 
+Definition matN := 'M[R]_(1%N + (n-1)%N).
 
-Definition P : 'M[R]_(1%N + (n-1)%N) := (@block_mx _ 1 (n-1)%N 1 (n-1)%N
+Definition P : matN := (@block_mx _ 1 (n-1)%N 1 (n-1)%N
                                            (const_mx 1)  (const_mx 1)
                                            (const_mx 1) ((-1)%:M)).
 
@@ -173,7 +176,6 @@ Lemma P_unit : P \in unitmx.
   have neg1M_unit m : ((-1)%:M : 'M[R]_m) \in unitmx
       by rewrite unitmxE neg1M_det; case: (odd m); 
     rewrite ?expr1 ?expr0 ?unitrN1 ?unitr1.
-
   
   rewrite unitmxE  det_block // neg1M_det (pinvmxE (neg1M_unit _))
     invmx_scalar det_mx11 invrN1  scalar_mxC
@@ -234,23 +236,24 @@ out.
 TODO: Check 'castmx' in matrix.v. I think it comes with lemmas for 
 substitution.
  *)
-Definition diag_vec : 'rV[R]_n :=
+
+(*Definition diag_vec : 'rV[R]_n :=
   @cast _ _ dim_is_n (fun t=> 'rV[R]_t)
        (row_mx ((k+n-(S O))%:R%:M : 'rV_1)
-          (@const_mx R _ _ (k-1)%:R : 'rV_(n-1))).
+          (@const_mx R _ _ (k-1)%:R : 'rV_(n-1))).*)
 
-Definition diag_vec' : 'rV[R]_(1 + (n-1)) :=
+Definition diag_vec : 'rV[R]_(1 + (n-1)) :=
   (row_mx ((k+n-(S O))%:R%:M : 'rV_1)
      (@const_mx R _ _ (k-1)%:R : 'rV_(n-1))).
 
-Definition D : 'M[R]_n := diag_mx diag_vec.
-Definition D' : 'M_(1+(n-1)) := diag_mx diag_vec'.
+(*Definition D : 'M[R]_n := diag_mx diag_vec.*)
+Definition adj2_diag : matN := diag_mx diag_vec.
 
 
-Lemma D_block_form : D' = @block_mx _ 1 (n-1)%N 1 (n-1)%N
+Lemma adj_diag_block_form : adj2_diag = @block_mx _ 1 (n-1)%N 1 (n-1)%N
                            (const_mx (k+n-1)%:R) 0
                            0            (k-1)%:R%:M.
-  rewrite /D' /diag_vec' diag_mx_row diag_const_mx.
+  rewrite /adj2_diag /diag_vec diag_mx_row diag_const_mx.
 
   suff oeua: (diag_mx (@scalar_mx _ 1 (k+n-1)%N%:R)) = 
                (@const_mx R 1 1 (k+n-1)%N%:R)
@@ -277,11 +280,11 @@ Qed.
   Qed.
   *)
 
-  Definition m' : 'M_(1 + (n-1))  := addmx (const_mx 1)
+  Definition adj2 : 'M_(1 + (n-1))  := addmx (const_mx 1)
                                       (@scalar_mx R _ (k-1)%:R).
-  (*Lemma m_is_ones_plus_I : m = m'.
+  (*Lemma m_is_ones_plus_I : m = adj2.
     (* Seems 3x longer than this should take *)
-    rewrite /m /m' /addmx.
+    rewrite /m /adj2 /addmx.
     apply/matrixP => i j; rewrite !mxE;
                      case: (i == j) => //=; clear i j.
     move: kge1; case: k => [//| k' _].
@@ -293,15 +296,15 @@ Qed.
   Qed.
    *)
 
-  Print m'.
-  Lemma m'_block_form :
-    m' = @block_mx _ 1 (n-1)%N 1 (n-1)%N
+  Print adj2.
+  Lemma adj2_block_form :
+    adj2 = @block_mx _ 1 (n-1)%N 1 (n-1)%N
            (const_mx k%:R)     (const_mx 1)
-           (const_mx 1)        (addmx (const_mx 1) (k - 1)%:R%:M).
+           (const_mx 1)        ((const_mx 1) + (k - 1)%:R%:M).
     apply/matrixP => i j.
     rewrite !mxE.
     case (split i) => [ (* forall o : 'I_1, *)
-        [[|m'] e]
+        [[|adj2] e]
       |] //=.
     (* TODO: Check the rowmx / colmx, submx lemmas in matrix.v
        AND/OR read chapter 7 of the math-comp book. *)
@@ -313,9 +316,9 @@ Qed.
      
   Admitted.
     
-Lemma D_m_P_eq : (P *m m') = (D' *m P).
+Lemma D_m_P_eq : P *m adj2 = adj2_diag *m P.
 
-  rewrite /P m'_block_form D_block_form
+  rewrite /P adj2_block_form adj_diag_block_form
     !mulmx_block !mul0mx !addr0 !add0r !mul_scalar_mx
     (mx11_scalar (const_mx (k + n - 1)%:R)) (mx11_scalar (const_mx 1))
     [in RHS](mx11_scalar (const_mx 1))
@@ -326,39 +329,150 @@ Lemma D_m_P_eq : (P *m m') = (D' *m P).
     (mx11_scalar (const_mx (n-1)%:R))
     !mxE (mx11_scalar (k%:R%:M + (n - 1)%:R%:M)) mxE  !(mxE _ _ 0 0) //=
     !mulr1n -natrD.
-  have kn_expr : (k + (n - 1))%N = (k + n -1)%N.
+  have -> : (k + (n - 1))%N = (k + n -1)%N.
   rewrite /subn /subn_rec /addn /addn_rec;
     move: nge1. case: n => [|n'] _ //=; lia.
-  rewrite kn_expr.
-  clear kn_expr field_has_char_zero.
+  clear field_has_char_zero.
   
-
   (* Bot left block: *)
   rewrite scalar_mxC mul_scalar_mx scalemx_const mulr1.
-  have bot_left_eq:
+  have ->:
     (@const_mx R (n-1) 1 k%:R + const_mx (-1)) = const_mx (k-1)%:R .
   apply/matrixP => i j; rewrite !mxE.
-  
   move: kge1; case: k => [//| k' _].
   by rewrite /subn /subn_rec //= PeanoNat.Nat.sub_0_r  [_.+1]/((1+k')%N) -/addn
     natrD addrC addrA (addNr 1) add0r.
-  rewrite bot_left_eq.
   (* bot left block done *)
 
-  (* TODO: CONTINUE FROM HERE
+  (* 1×1^T = ones; used in 2 places. *)
+  have -> : (@const_mx R (n-1) (n-1) 1) =
+              (const_mx 1 : 'cV__) *m (const_mx 1 : 'rV__).
+  apply/matrixP => i j; rewrite !mxE  (eq_bigr (fun=>1))  ?big_const_ord
+                          ?iter_addr ?addr0 //=.
+  by move=> i' _; rewrite !mxE mulr1.
+  (* end proof  of 1×1 = ones*)
 
-     Left (in this proof):
-        - ones : 'M_n = col_ones *m row_ones
-        - use that to rewrite the top right and bot right expression
-     Left (in other parts):
-        - block form for 'm' (possibly look into 'cast' and submx
-        - ring characteristic; rewrite the 
+  (* Top right piece *)
+  rewrite mulmxDr  [in LHS]mulmxA dotmul
+    (mx11_scalar (const_mx _)) (mxE _ _ 0 0)
+    mul_scalar_mx  mul_mx_scalar !scalemx_const -!raddfD //=.
+
+  have -> : 1 + ((n - 1)%:R * 1 + (k - 1)%:R * 1) = ((k + n - 1)%:R :R).
+  rewrite !mulr1  -{1}[in 1](mulr1n 1) -!natrD.
+  suff -> : (1 + (n - 1 + (k - 1)))%N = (k + n - 1)%N by [].
+  by move: nge1 kge1; case n => [|n' _] //=; case k => [|k' _] //=;
+      rewrite /subn /subn_rec /addn /addn_rec; lia.
+  (* Top right piece done *)
+
+  (* Bot right piece *)
+  by rewrite scaleN1r [in -(_ + _)] raddfD //=  addrA addrN add0r mulrN1
+     -mulNrn -raddfN //= mulNrn.
+Qed.
+
+Lemma diagonalizable_m : adj2 ~_P adj2_diag.
+  by apply/simmxW; [ rewrite row_free_unit; exact P_unit
+                     |  exact D_m_P_eq].
+Qed.
+
+Lemma char_poly_adj2: char_poly adj2 =
+  ('X - ((k + n - 1)%:R)%:P) * ('X - ((k - 1)%:R)%:P) ^+ (n - 1).
+Proof.
+  About simmx_charpoly.
+  rewrite (simmx_charpoly P_unit diagonalizable_m).
+  have adj2_trig : is_trig_mx adj2_diag
+    by apply: (is_diag_mx_is_trig (diag_mx_is_diag _)).
+  rewrite (char_poly_trig adj2_trig).
+
+  rewrite (@big_split_ord _ _ _ 1%N (n-1) _ _) //= adj_diag_block_form
+    (eq_bigr (fun=> ('X-((k + n - 1)%:R :R) %:P))); last first.
+  {
+    move=>i _; rewrite block_mxEul.
+    suff -> : (@const_mx R 1 1 (k + n - 1)%:R i i) = (k + n - 1)%:R by [].
+    by rewrite mxE.
+  }
+
+  rewrite [in X in _*X](eq_bigr (fun=> ('X-((k - 1)%:R) %:P))); last first.
+  {
+    move=>i _; rewrite block_mxEdr.
+    suff -> : ((k - 1)%:R%:M i i) = ((k - 1)%:R :R) by [].
+    rewrite mxE //=.
+    have -> : (i==i) by apply/eqP .
+    by rewrite mulr1n.
+  }
+
+  by rewrite !big_const_ord !iter_mulr !mulr1 expr1.
+Qed.
+
+Definition is_square_root A := A *m A = adj2.
+
+
+  (*
+
+Next step: show a lemma that relates 'char_poly A^2 ' to 'char_poly A'
+for alg closed fields or assuming both split into linear factors. One
+gets its roots squared. You expand det (A^2 - x^2I) = det(A-xI) ×
+det(A+xI) and equate both sides.
+
+Isn't needed:
+Lemma square_root_commutes A :
+  is_square_root A -> A *m adj2 = adj2 *m A.
+  by move=> <-; rewrite mulmxA.
+Qed.
+
+   
+
+Record SquareRootDiag {A} (isq: is_square_root A) : Type := Dummy {
+                                    D: matN;
+                                    D2: matN;
+                                    Q: matN;
+                                    Dsq : D *m D = D2;
+                                    A_sim_D : A ~_Q D;
+                                    adj_sim_D2 : adj2 ~_Q D2
+                                  }.
+
+Lemma square_root_diag A (isq: is_square_root A) :
+  SquareRootDiag isq.
+
+  
+
+  Check (simmxRL P_unit).
+  have m_expr : adj2 = (invmx P) *m adj2_diag *m P.
+   {rewrite (simmxLR P_unit diagonalizable_m) /conjmx.
+    have iP_u : (invmx P) \in unitmx by rewrite (unitmx_inv P); exact P_unit.
+    by rewrite (pinvmxE iP_u) invmxK.
+   }
+   rewrite -A_sqr in m_expr.
+
+   Check (codiagonalizableP [:: A; adj2] ).
+
+   have aoue := 
+   
+        Search invmx.
+    have adj2_diag_conj : adj2_diag ~_(invmx P) adj2.
+
+  apply /simmxRL .
+  {  rewrite unitmx_inv; exact P_unit.}
+  {
+    rewrite (simmxLR P_unit diagonalizable_m) /conjmx.
+    
+  }
+    Search pinvmx invmx.
+    Check (unitmx_inv P P_unit).
+    
+    Check .
+    Search (_ ~__ _).
+    Search invmx.
+
+    mulVpmx: forall [F : fieldType] [m n : nat] [A : 'M_(m, n)],
+        row_full A -> pinvmx A *m A = 1%:M
+mulmxVp:
+    
+  
+  
+  rewrite /(simmxRL P_unit diagonalizable_m).
+  
+
+Search "sqrt" -BinNums.Z -BinNums.N -nat.
+Print ssrnum.Num.ClosedField.
    *)
 
-(*
-Lemma D_diag_form : D = diag_mx (
-      row_mx ((k+n-(S O))%:R%:M : 'rV_1)
-        (@const_mx R _ _ (k-1)%:R : 'rV_(n-1))).
-Print D. 
-Lemma diagonalizable_m : A ~_P B
-*)
